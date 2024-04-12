653cc95069b0f51f22702acda3399508b4618f81
anestxs <crtrever@gmail.com>
2024-04-12 23:40:57 +0300

    Update 4.ts

--- src/4.ts
+++ src/4.ts
@@ -32,9 +32,10 @@ class Person {
 }
 
 abstract class House {
-  door: boolean;
-  key: Key;
-  tenants: Person[] = [];
+  protected door: boolean = false;
+  private tenants: Person[] = [];
+
+  constructor(protected key: Key) {}
   comeIn(person: Person): void {
     if (this.door) {
       this.tenants.push(person);
@@ -44,10 +45,6 @@ abstract class House {
 }
 
 class MyHouse extends House {
-  constructor(key: Key) {
-    super();
-    this.key = key;
-  }
   openDoor(key: Key): void {
     if (key.getSignature() === this.key.getSignature()) {
       this.door = true;
